45b4d7fe7af354d118232fdf74c8a26da25ef26e
mack-G <hggamahewage@gmail.com>
2015-01-12 12:03:10 -0800

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to RampDoc!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-
             private
 
             def user_params
