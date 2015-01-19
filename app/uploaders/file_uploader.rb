de836090c2e5b7b9de7c9d9c01dd81c550bbc7a5
mack-G <hggamahewage@gmail.com>
2015-01-18 16:10:51 -0800

    Local AWS configured

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -4,10 +4,10 @@ class FileUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  #storage :file
 
   storage :fog
 
